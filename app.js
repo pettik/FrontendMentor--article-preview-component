7299d8191edb9e77fa101285f1a94a60df561be4
pettik <65282144+pettik@users.noreply.github.com>
2023-03-14 18:39:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const tlacitkoSipka = document.querySelector('.article-text-box__share');
 const shareBox = document.querySelector('.sharing-box');
 
-
-tlacitkoSipka.addEventListener('click',function(){
-    tlacitkoSipka.classList.toggle('aktivni');
-    shareBox.classList.toggle('zmiz');
-})
+tlacitkoSipka.addEventListener('click', function () {
+  tlacitkoSipka.classList.toggle('aktivni');
+  shareBox.classList.toggle('zmiz');
+});
